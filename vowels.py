366fa4a99c466606fff7039faf8ce027433daca3
Terry Enigbonjaiye <terryenigbonjaiye@carehearted.co.uk>
2022-06-12 22:46:34 +0100

    adding vowels

---
+++ vowels.py
@@ -0,0 +1,9 @@
+vowels = ["a", "e", "i", "o", "u"]
+word = input("Provide a  word to search for vowels")
+found = []
+for latter in word:
+    if latter in vowels:
+        if latter not in found:
+            found.append(latter)
+for vowels in found:
+    print(vowels)
