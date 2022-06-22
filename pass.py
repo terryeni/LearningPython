7b9921c88c139d4c1db7b7cf9f2633c10b13885c
Terry Enigbonjaiye <terryenigbonjaiye@carehearted.co.uk>
2022-06-22 23:43:27 +0100

    pass

---
+++ pass.py
@@ -0,0 +1,13 @@
+print("Enter your name: ", input())
+name = input()
+password = ("swordfish")
+
+if name == "Mary":
+    print("Hello, Mary")
+    if password == 'swordfish':
+        print("Access granted.")
+    else:
+        print("Wrong password")
+
+
+quit()
